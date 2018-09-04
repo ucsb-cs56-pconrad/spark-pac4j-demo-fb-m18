aa2ec84bd879929c158a30de2a0c276c288645c5
Phill Conrad <pconrad@cs.ucsb.edu>
2018-09-03 23:28:48 -0700

    update nav

--- src/main/java/org/pac4j/demo/spark/SparkPac4jDemo.java
+++ src/main/java/org/pac4j/demo/spark/SparkPac4jDemo.java
@@ -15,7 +15,6 @@ import org.pac4j.http.client.indirect.FormClient;
 
 import org.pac4j.core.http.HttpActionAdapter;
 
-import org.pac4j.saml.client.SAML2Client;
 import org.pac4j.sparkjava.CallbackRoute;
 import org.pac4j.sparkjava.LogoutRoute;
 import org.pac4j.sparkjava.SecurityFilter;
@@ -112,57 +111,6 @@ public class SparkPac4jDemo {
 	    
 	Spark.get("/", SparkPac4jDemo::index, templateEngine);
 
-	final CallbackRoute callback = new CallbackRoute(config, null, true);
-	//callback.setRenewSession(false);	
-	Spark.get("/callback", callback);
-	Spark.post("/callback", callback);
-	
-	final SecurityFilter facebookFilter =
-	    new SecurityFilter(config, "FacebookClient", "", "excludedPath");
-	
-	Spark.before("/facebook", facebookFilter);
-	Spark.before("/facebook/*", facebookFilter);
-	Spark.before("/facebookadmin",
-		     new SecurityFilter(config, "FacebookClient", "admin"));
-	Spark.before("/facebookcustom",
-		     new SecurityFilter(config, "FacebookClient", "custom"));
-
-	Spark.before("/protected", new SecurityFilter(config, null));
-
-
-	Spark.get("/facebook", SparkPac4jDemo::protectedIndex, templateEngine);
-        Spark.get("/facebook/notprotected", SparkPac4jDemo::protectedIndex, templateEngine);
-	Spark.get("/facebookadmin", SparkPac4jDemo::protectedIndex, templateEngine);
-	Spark.get("/facebookcustom", SparkPac4jDemo::protectedIndex, templateEngine);
-
-	Spark.get("/protected", SparkPac4jDemo::protectedIndex, templateEngine);
-	
-	final LogoutRoute localLogout =
-	    new LogoutRoute(config, "/?defaulturlafterlogout");
-	localLogout.setDestroySession(true);
-	Spark.get("/logout", localLogout);
-	final LogoutRoute centralLogout = new LogoutRoute(config);
-	centralLogout.setDefaultUrl("http://localhost:" + getPortAsString() +
-				    "/?defaulturlafterlogoutafteridp");
-	centralLogout.setLogoutUrlPattern("http://localhost:" +
-					  getPortAsString() + "/.*");
-	centralLogout.setLocalLogout(false);
-	centralLogout.setCentralLogout(true);
-	centralLogout.setDestroySession(true);
-	Spark.get("/centralLogout", centralLogout);
-	Spark.get("/forceLogin", (rq, rs) -> forceLogin(config, rq, rs));
-
-	/*before("/body", (request, response) -> {
-	  logger.debug("before /body");
-	  });*/
-	//before("/body", new SecurityFilter(config, "AnonymousClient"));
-
-	Spark.before("/body", new SecurityFilter(config, "HeaderClient"));
-	Spark.post("/body", (request, response) -> {
-		logger.debug("Body: " + request.body());
-		return "done: " + getProfiles(request, response);
-	    });
-
 	Spark.exception(Exception.class, (e, request, response) -> {
 		logger.error("Unexpected exception", e);
 		response.body(templateEngine.render(new ModelAndView(new HashMap<>(), "error500.mustache")));
@@ -178,30 +126,10 @@ public class SparkPac4jDemo {
     }
 
 
-    private static ModelAndView protectedIndex(final Request request, final Response response) {
-	final Map<String,Object> map = new HashMap<String,Object>();
-	map.put("profiles", getProfiles(request, response));
-	return new ModelAndView(map, "protectedIndex.mustache");
-    }
-
     private static List<CommonProfile> getProfiles(final Request request, final Response response) {
 	final SparkWebContext context = new SparkWebContext(request, response);
 	final ProfileManager<CommonProfile> manager = new ProfileManager<CommonProfile>(context);
 	return manager.getAll(true);
     }
 
-    private static ModelAndView forceLogin(final DemoConfig<Object,SparkWebContext> config, final Request request, final Response response) {
-        final SparkWebContext context = new SparkWebContext(request, response);
-        final String clientName = context.getRequestParameter(Clients.DEFAULT_CLIENT_NAME_PARAMETER);
-	final Client client = config.getClients().findClient(clientName);
-	HttpAction action;
-	try {
-	    action = client.redirect(context);
-	} catch (final HttpAction e) {
-	    action = e;
-	}
-	HttpActionAdapter<Object,SparkWebContext> haa = config.getHttpActionAdapter_FIXED();
-	haa.adapt(action.getCode(), context);
-	return null;
-    }
 }
